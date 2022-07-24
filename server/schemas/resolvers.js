52ed5ae0da0c3a7d6aef43a43096f7a5715a42e9
Ashley <lauraashleywalker4@gmail.com>
2022-07-24 09:04:12 -0400

    update resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,41 +1,66 @@
-const { AuthenticationError }= require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToke } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         me: async (_parent, _args, context) => {
-            if (context.user){
-                const userData = await User.findOne({ _id: context.user._id})
-                .select('_v -password')
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('_v -password')
 
                 return userDatal
             }
             throw new AuthenticationError('You are NOT logged in.');
         }
     },
-Mutation: {
-    addUser: async(_parent, args) => {
-        const user = await User.create(args);
-        const token = signToken(user);
+    Mutation: {
+        addUser: async (_parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
-        return {token, user};
-    },
+            return { token, user };
+        },
 
-    login: async (parent, { email, password}) => {
-        const user = await User.findOne({ email });
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
-        if (!user){
-            throw new AuthenticationError('Incorrect credentials');
-        }
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
 
-        const correctPw = await user.isCorrectPassword(password);
+            const correctPw = await user.isCorrectPassword(password);
 
-        if (!correctPw) {
-            throw new AuthenticationError('Incorrect credentails');
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentails');
+            }
+
+            const token = singleToken(user);
+            return { token, user };
+        },
+        saveBook: async (_parent, { input }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addtoSet: { savedBooks: input } },
+                    { new: true }
+                );
+                return updateUser;
+            }
+            throw new AuthenticationsError('Not Logged in');
+        },
+        removeBook: async (_parent, { bookId }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You are not logged in');
         }
     }
+};
 
-}
-
-}
+module.exports = resolvers;
